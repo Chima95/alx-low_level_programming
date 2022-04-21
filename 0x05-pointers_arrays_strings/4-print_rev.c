4654329ea840d0e74b1766c284a240d9f6ab74c1
Chima95 <nzeamaluchima@gmail.com>
2022-04-21 23:29:41 +0100

    4 - task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+/**
+ * print_rev - prints a string in reserve order
+ * @s: A pointer to an int that will be changed
+ *
+ * Return: void which means our answer is correct
+ */
+
+void print_rev(char *s)
+{
+int i:
+
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
