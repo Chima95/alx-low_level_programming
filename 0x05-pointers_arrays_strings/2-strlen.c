046c310c1212b93949dc86ea80d69784ab1d244c
Chima95 <nzeamaluchima@gmail.com>
2022-04-21 06:17:47 +0100

    2 commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ *            using two input parameters
+ *
+ * @a: input parameter 1
+ * @b: input parameter 2
+ *
+ * Return: Nothing
+ */
+
+int _strlen(char *s)
+{
+int u;
+
+u = 0;
+while(s[u] != '\0')
+{
+u++;
+}return (u);
+}
