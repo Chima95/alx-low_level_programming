653d57962a5b03f875d27f4f1c25cd752864d0a0
Chima95 <nzeamaluchima@gmail.com>
2022-04-21 19:07:24 +0100

    adjustment

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,19 @@
+/**
+ *_puts - print a string
+ *@str: A pointer to an int that will be changed
+ *
+ *Return: void which means our answer is correct
+ */
+
+void _puts(char *str)
+{
+
+        int i;
+	
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+ putchar('\n');
+return;
+}
